958de4f802fa2207c47c367225d2686fac9392be
minchaehong <minchaehong2@gmail.com>
2025-07-17 10:27:07 +0900

    fixed-api-8

--- src/api/API.js
+++ src/api/API.js
@@ -26,7 +26,7 @@ export function startPolling(newValue, onUpdate) {
   active = true;
 
   // 🔷 최초 값 전송
-  axios.post(`${BASE_URL}/command${newValue}`,null, { timeout: 2000 })
+  axios.post(`${BASE_URL}/command/${newValue}`,null, { timeout: 2000 })
     .then(() => {
       console.log(`✅ 초기 value(${newValue}) 전송 성공`);
       poll(); // 폴링 시작
